da3167e57b3ac7ed52cf694c868d7349f239405d
liangshb <liangshb@outlook.com>
2020-10-21 10:48:06 +0800

    Test for GitHub plugin

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello, DevOps" << '\n';
     std::cout << "Hello, Jenkins" << '\n';
     std::cout << "Hello, cmake" << '\n';
+    std::cout << "Hello, GitHub" << '\n';
     return 0;
 }
